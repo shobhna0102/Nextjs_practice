98af2d832da5d0afcb00354a3654c994dad31ee2
shobhna0102 <shobhnabaraiya@webcluesinfotech.com>
2022-08-08 18:31:14 +0530

    nextjs demo task commit

---
+++ nextjs_demo/pages/contact.js
@@ -0,0 +1,31 @@
+import Navbar from "../components/Navbar";
+import styles from "../styles/contact.module.css";
+{
+  /* <head>
+  <style>
+  </style>
+</head>; */
+}
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      {/* <h1 style={{ color: "green" }}>Hello World my contact </h1> */}
+      <h1>Hello World my contact </h1>
+      <p className="intro"></p>
+
+      <style jsx>
+        {`
+          h1 {
+            color: pink;
+          }
+          .intro {
+            color: blue;
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+export default Contact;
---
+++ nextjs_demo/pages/home.js
@@ -0,0 +1,9 @@
+const home = () => {
+    return (
+      <>
+        <h1>Hello World my Home </h1>
+      </>
+    );
+  };
+  
+  export default home;
